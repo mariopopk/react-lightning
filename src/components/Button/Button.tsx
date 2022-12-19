db0cd30dfbd275acc5ead37239f7e4f25c0c7d31
mariopopk <travesura-realizada@hotmail.com>
2022-12-18 21:07:29 -0700

    Variable setup

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,13 +1,21 @@
 import React, { ReactNode } from "react";
 import styles from "./Button.css";
 import cx from "classnames";
+import { Variant } from "../../theme/variables";
 
 export interface ButtonProps {
   children?: ReactNode;
+  variant?: Variant;
 }
 
-export function Button({ children }: ButtonProps) {
-  return (
-    <button className={cx(styles.btn, styles.btnPrimary)}>{children}</button>
+export function Button({ children, variant = "primary" }: ButtonProps) {
+  const classes = cx(
+    styles.btn,
+    {
+      [styles[variant]]: variant,
+    }
+    // className
   );
+
+  return <button className={classes}>{children}</button>;
 }
