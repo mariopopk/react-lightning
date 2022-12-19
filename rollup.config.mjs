df06b420ddc1308aa9694df3e4213cf7f5bfc36f
mariopopk <travesura-realizada@hotmail.com>
2022-12-19 16:24:49 -0700

    Rollup updated configuration

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,22 +2,21 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
-import packageJson from "./package.json" assert { type: "json" };
-
 import postcss from "rollup-plugin-postcss";
 import postcssModulesValues from "postcss-modules-values";
 
 export default [
   {
     input: "src/index.ts",
+    external: ["react"],
     output: [
       {
-        file: packageJson.main,
+        file: "dist/cjs/index.js", //should be same as packageJson.main
         format: "cjs",
         sourcemap: true,
       },
       {
-        file: packageJson.module,
+        file: "dist/esm/index.js", //should be same as packageJson.module
         format: "esm",
         sourcemap: true,
       },
@@ -37,5 +36,6 @@ export default [
     output: [{ file: "dist/index.d.ts", format: "esm" }],
     plugins: [dts()],
     external: [/\.css$/],
+    watch: true,
   },
 ];
