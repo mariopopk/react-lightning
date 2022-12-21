715a5fc7942340b0f2607a810547279bfb239064
mariopopk <travesura-realizada@hotmail.com>
2022-12-20 17:15:45 -0700

    Removed unused files

--- types/components/IconButton/IconButton.d.ts
+++
@@ -1,9 +0,0 @@
-import { ReactNode } from "react";
-import { Variant, Size, Color } from "../../theme/variables";
-export interface IconButtonProps {
-    children?: ReactNode;
-    variant?: Variant;
-    size?: Size;
-    color?: Color;
-}
-export declare function IconButton({ children, variant, size, color, ...rest }: IconButtonProps): JSX.Element;
