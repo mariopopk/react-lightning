d33f0829a157e58beb043004beed4d8dd5ceb419
mariopopk <travesura-realizada@hotmail.com>
2022-12-21 04:53:09 -0700

    Coverage for Button and FormExpand components

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -4,7 +4,7 @@ import { render } from "@testing-library/react";
 import Button from "./Button";
 
 describe("Button", () => {
-  test("renders the Button component", () => {
+  it("renders the Button component", () => {
     render(<Button children="Hello world!" />);
   });
 });
--- src/components/Form/FormExpand.test.tsx
+++ src/components/Form/FormExpand.test.tsx
@@ -1,13 +1,37 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import {
+  fireEvent,
+  render,
+  screen,
+  waitForElementToBeRemoved,
+} from "@testing-library/react";
 import FormExpand from "./FormExpand";
+import userEvent from "@testing-library/user-event";
 
-describe("FormExpand", () => {
-  test("renders the FormExpand component", () => {
+describe("<FormExpand/>", () => {
+  it("renders succesfully", () => {
     const { getByText } = render(
       <FormExpand label="Search" displayLabel="Search" />
     );
-
     expect(getByText("Search")).toBeInTheDocument();
+  });
+
+  it("expands (displays) input field when it receives focus via click or keyboard interaction. Input disappears when blur occurs", async () => {
+    const user = userEvent.setup();
+    const { getByRole, queryByRole } = render(
+      <FormExpand label="Search" displayLabel="Search" />
+    );
+    expect(getByRole("button")).toBeInTheDocument();
+    expect(queryByRole("textbox")).not.toBeInTheDocument();
+
+    await user.click(screen.getByRole("button", { name: /Search/i }));
+
+    expect(getByRole("textbox")).toBeInTheDocument();
+    expect(queryByRole("button")).not.toBeInTheDocument();
+
+    fireEvent.blur(getByRole("textbox"));
+
+    expect(getByRole("button")).toBeInTheDocument();
+    expect(queryByRole("textbox")).not.toBeInTheDocument();
   });
 });
