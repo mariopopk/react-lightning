382e5a2b1d69c59ba53237ea17f64942b5877563
mariopopk <travesura-realizada@hotmail.com>
2023-01-01 18:51:42 -0700

    Change of plans with hook design

--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -4,6 +4,7 @@ import React, {
   useEffect,
   useRef,
   useState,
+  RefObject,
 } from "react";
 import styles from "./Carousel.css";
 import cx from "classnames";
@@ -22,44 +23,67 @@ export interface CarouselItemProps {
   img: string;
 }
 
-export interface useSlideCarouselProps {}
+export interface useSlideCarouselProps {
+  inScreenItems: number;
+  totalItems: number;
+  carouselRef: RefObject<HTMLDivElement>;
+}
 
-const useSlideCarousel = ({}: useSlideCarouselProps) => {};
-
-export default function Carousel({ items, inScreenItems = 4 }: CarouselProps) {
-  // style
-  const itemMarginXInRem: number = 0.25;
-
-  // State
-  const carouselRef = useRef<HTMLDivElement>(null);
+function useSlideCarousel({
+  inScreenItems,
+  totalItems,
+  carouselRef,
+}: useSlideCarouselProps) {
   const [containerWidth, setContainerWidth] = useState<number>(0);
-  const [position, setPosition] = useState({
-    x: 0,
-    y: 0,
-  });
+  const [positionX, setPositionX] = useState(0);
+  // Assumes each item is the same width
+  const totalWidth = (containerWidth / inScreenItems) * totalItems;
+  const offScreenItemsWidth = totalWidth - containerWidth;
+  const itemWidth = totalWidth / totalItems;
 
+  // Resizing
   useEffect(() => {
     // TODO: Throttle for performance
     const getCarouselWidth = () => {
       setContainerWidth(carouselRef.current?.clientWidth || 0);
     };
-
     getCarouselWidth();
-
     window.addEventListener("resize", getCarouselWidth);
-
     return () => {
       window.removeEventListener("resize", getCarouselWidth);
     };
   }, []);
 
-  let { moveProps } = useMove({
-    onMove(e) {
-      // Assumes each item is the same width
-      const totalWidth = (containerWidth / inScreenItems) * items.length;
-      const offScreenItemsWidth = totalWidth - containerWidth;
+  function enforceBounds(x: number) {
+    const lowerBound = 0;
+    const upperBound = -offScreenItemsWidth;
 
-      setPosition(({ x, y }) => {
+    // Bounds
+    if (x < upperBound) x = upperBound;
+    if (x > lowerBound) x = lowerBound;
+
+    return x;
+  }
+
+  const moveOne = (direction: "left" | "right") =>
+    setPositionX((positionX) => {
+      const moveDirection = direction === "left" ? 1 : -1;
+      let currentItem = Math.round(positionX / itemWidth);
+
+      currentItem += moveDirection;
+      return enforceBounds(currentItem * itemWidth);
+    });
+
+  function moveMany(direction: "left" | "right") {
+    const moveDirection = direction === "left" ? 1 : -1;
+    let currentItem = Math.round(positionX / itemWidth);
+    currentItem += moveDirection * inScreenItems;
+    return enforceBounds(currentItem * itemWidth);
+  }
+
+  const { moveProps } = useMove({
+    onMove(e) {
+      setPositionX((x) => {
         // Normally, we want to allow the user to continue
         // dragging outside the box such that they need to
         // drag back over the ball again before it moves.
@@ -69,26 +93,34 @@ export default function Carousel({ items, inScreenItems = 4 }: CarouselProps) {
         // then using the arrow keys works as expected.
         if (e.pointerType === "keyboard") {
           // x = clamp(x);
-          // y = clamp(y);
         }
 
         // Update x value
         x += e.deltaX;
 
-        const lowerBound = 0;
-        const upperBound = -offScreenItemsWidth;
-
-        // Bounds;
-        if (x < upperBound) {
-          x = upperBound;
-        }
-        if (x > lowerBound) {
-          x = lowerBound;
-        }
-
-        return { x, y };
+        return enforceBounds(x);
       });
     },
+  });
+
+  return {
+    moveProps: {
+      ...moveProps,
+    },
+    positionX,
+    moveOne,
+    moveMany,
+  };
+}
+
+export default function Carousel({ items, inScreenItems = 4 }: CarouselProps) {
+  // State
+  const carouselRef = useRef<HTMLDivElement>(null);
+
+  const { moveProps, positionX, moveOne, moveMany } = useSlideCarousel({
+    carouselRef,
+    inScreenItems,
+    totalItems: items.length,
   });
 
   return (
@@ -97,7 +129,14 @@ export default function Carousel({ items, inScreenItems = 4 }: CarouselProps) {
         className={cx(styles["carousel-button"], styles.left)}
         style={{ left: 0 }}
       >
-        <Button backgroundColor="light">Previous</Button>
+        <Button
+          style={{ width: "100%" }}
+          onPress={() => moveOne("left")}
+          backgroundColor="transparent"
+          color="light"
+        >
+          {"<"}
+        </Button>
       </div>
 
       <div
@@ -105,32 +144,38 @@ export default function Carousel({ items, inScreenItems = 4 }: CarouselProps) {
         {...moveProps}
         style={{
           whiteSpace: "nowrap",
-          transform: `translate(${position.x}px, 0px)`,
+          transform: `translate(${positionX}px, 0px)`,
+          transition: "ease-out",
+        }}
+        onWheel={(e) => {
+          const direction = e.deltaX === 1 ? "right" : "left";
+          console.log(direction, "throttle?");
+          throttle(() => {
+            console.log("once");
+          }, 1000);
         }}
         tabIndex={0}
       >
         {items.map(({ id, link, img }: CarouselItemProps, i: number) => {
           return (
             <div
+              key={id}
               className={styles["carousel-item"]}
               style={{
-                width: `${
-                  (100 - (inScreenItems - 1) * itemMarginXInRem) / inScreenItems
-                }%`,
-                marginRight: i > inScreenItems ? "0" : `${itemMarginXInRem}rem`,
+                width: `${100 / inScreenItems}%`,
               }}
             >
               <AspectRatioBox aspectRatio={{ w: 16, h: 9 }}>
-                {/* <a href="/"> */}
-                <img
-                  style={{
-                    height: "100%",
-                    objectFit: "cover",
-                    width: "100%",
-                  }}
-                  src={img}
-                />
-                {/* </a> */}
+                <div style={{ margin: "0.1rem" }}>
+                  <img
+                    style={{
+                      height: "100%",
+                      objectFit: "cover",
+                      width: "100%",
+                    }}
+                    src={img}
+                  />
+                </div>
               </AspectRatioBox>
             </div>
           );
@@ -141,7 +186,15 @@ export default function Carousel({ items, inScreenItems = 4 }: CarouselProps) {
         className={cx(styles["carousel-button"], styles.right)}
         style={{ right: 0 }}
       >
-        {/* <Button backgroundColor="light">Next</Button> */}
+        <Button
+          style={{ width: "100%" }}
+          onPress={() => moveOne("right")}
+          backgroundColor="transparent"
+          color="light"
+        >
+          {/* Next */}
+          {">"}
+        </Button>
       </div>
     </div>
   );
